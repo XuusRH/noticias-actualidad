871bc5af506823c2266790ee21686a86b7e42bf6
XuusRH <andreagarciasola7@gmail.com>
2025-10-09 03:51:01 +0200

    Update news.js

--- api/news.js
+++ api/news.js
@@ -1,6 +1,6 @@
 // Proxy para NewsAPI
 export default async function handler(req, res) {
-  const API_KEY = "TU_API_KEY_DE_NEWSAPI"; // 👈 Pega aquí tu clave de NewsAPI
+  const API_KEY = "bcda57c6ef4146a48995b71719060260"; // 
 
   const url = `https://newsapi.org/v2/everything?q=economia+OR+bolsa+OR+inversion+OR+criptomonedas&language=es&pageSize=10&apiKey=${API_KEY}`;
 
